6bd7dfe2a51957911d910e6748824f35a546c29c
Timmy <timmynielstim1111@gmail.com>
2020-04-27 01:24:12 +0200

    lo

--- app/Brain.py
+++ app/Brain.py
@@ -1,14 +1,14 @@
 def message(data):
     print(data)
 
-def avoidwall(date):
+def avoidwall(data):
     head_x = data['body'][0]['x']
     head_y = data['body'][0]['y']
     board_width = data['width']
-    board_height = date['height']
+    board_height = data['height']
 
     if head_x == 1:
-       return 'down'
+        return 'down'
     if head_x == board_width-1:
         return 'up'
     if head_y == 1:
