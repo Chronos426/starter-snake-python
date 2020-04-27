76775520285935621a20bad93ac204fc15772270
Timmy <timmynielstim1111@gmail.com>
2020-04-27 23:06:27 +0200

    lo

--- app/Brain.py
+++ app/Brain.py
@@ -10,18 +10,61 @@ def message(data):
     print(data)
 
 def avoidwall(data):
-    head_x = data['you']['body'][0]['x']
-    head_y = data['you']['body'][0]['y']
+    head_x_1 = data['you']['body'][0]['x']
+    head_y_1 = data['you']['body'][0]['y']
+    head_x_2 = data['you']['body'][1]['x']
+    head_y_2 = data['you']['body'][1]['y']
     board_width = data['board']['width']
     board_height = data['board']['height']
 
-    if head_x == 1:
-        return 'down'
-    if head_x == board_width-1:
-        return 'up'
-    if head_y == 1:
-        return 'right'
-    if head_y == board_height -1:
-        return 'right'
 
+    if head_x_1 > head_x_2:
+        direction = 'E'
+        front_tim = [head_x_1 + 1, head_y_1]
+        left_tim = [head_x_1, head_y_1 - 1]
+        right_tim = [head_x_1, head_y_1 + 1]
+    elif head_x_1 < head_x_2:
+        direction = 'O'
+        front_tim = [head_x_1 - 1, head_y_1]
+        left_tim = [head_x_1, head_y_1 + 1]
+        right_tim = [head_x_1, head_y_1 - 1]
+    elif head_y_1 < head_y_2:
+        direction = 'N'
+        front_tim = [head_x_1, head_y_1 - 1]
+        left_tim = [head_x_1 - 1, head_y_1]
+        right_tim = [head_x_1 + 1, head_y_1]
+    else:
+        direction = 'S'
+        front_tim = [head_x_1, head_y_1 + 1]
+        left_tim = [head_x_1 + 1, head_y_1]
+        right_tim = [head_x_1 - 1, head_y_1]
 
+    if head_x_1 == 0 and direction == 'O':
+        if head_y_1 == 0:
+            return 'down'
+        else:
+            return 'up'
+    elif head_y_1 == 0 and direction == 'N':
+        if head_x_1 == 0:
+            return 'right'
+        else:
+            return 'left'
+    elif head_y_1 == board_height - 1 and direction == 'S':
+        if head_x_1 == 0:
+            return 'right'
+        else:
+            return 'left'
+    elif head_x_1 == board_width - 1 and direction == 'E':
+        if head_y_1 == 0:
+            return 'down'
+        else:
+            return 'up'
+    else:
+        if direction == 'E':
+            return 'right'
+        elif direction == 'O':
+            return 'left'
+        elif direction == 'N':
+            return 'up'
+        elif direction == 'S':
+            return 'down'
