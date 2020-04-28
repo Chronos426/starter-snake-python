c4eacbf7b89fb3fb16b602f8f053abb7f4bff450
Timmy <timmynielstim1111@gmail.com>
2020-04-28 17:38:29 +0200

    lo

--- app/Brain.py
+++ app/Brain.py
@@ -1,12 +1,12 @@
-from  Nombre_passage_algo import nombre_case
+#from  Nombre_passage_algo import nombre_case
 
-data = {"you": {"body": [{"y": 3, "x": 9}, {"y": 4, "x": 9}, {"y": 5, "x": 9}], "health": 94,
-               "id": "gs_WdqV3GF3Vj6TDbjffScYMgG4", "shout": "", "name": "SnakeOne"}, "board": {
-    "food": [{"y": 7, "x": 0}, {"y": 7, "x": 2}, {"y": 6, "x": 1}, {"y": 2, "x": 3}, {"y": 5, "x": 1}], "snakes": [
-        {"body": [{"y": 3, "x": 9}, {"y": 4, "x": 9}, {"y": 5, "x": 9}], "health": 94,
-         "id": "gs_WdqV3GF3Vj6TDbjffScYMgG4", "shout": "", "name": "SnakeOne"}], "width": 11, "height": 11},
-   "turn": 6, "game": {"id": "08076fc9-07f0-462c-9ec7-59b3c8fdb08d"}}
-chemin = [0, 0, 0, 0]
+#data = {"you": {"body": [{"y": 3, "x": 9}, {"y": 4, "x": 9}, {"y": 5, "x": 9}], "health": 94,
+ #              "id": "gs_WdqV3GF3Vj6TDbjffScYMgG4", "shout": "", "name": "SnakeOne"}, "board": {
+ #   "food": [{"y": 7, "x": 0}, {"y": 7, "x": 2}, {"y": 6, "x": 1}, {"y": 2, "x": 3}, {"y": 5, "x": 1}], "snakes": [
+#        {"body": [{"y": 3, "x": 9}, {"y": 4, "x": 9}, {"y": 5, "x": 9}], "health": 94,
+ #        "id": "gs_WdqV3GF3Vj6TDbjffScYMgG4", "shout": "", "name": "SnakeOne"}], "width": 11, "height": 11},
+ #  "turn": 6, "game": {"id": "08076fc9-07f0-462c-9ec7-59b3c8fdb08d"}}
+#chemin = [0, 0, 0, 0]
 
 def message(data):
     print(data)
@@ -90,7 +90,7 @@ def colision_tete(data, chemin):
         if data['board']['snakes'][i]['id'] != data['you']['id']:
 
             snake_position = snake_position.append(data['board']['snakes'][i]['body'][0]['x'])
-            print("snake_position: " + snake_position)
+           #print("snake_position: " + snake_position)
             snake_position[i*8] -= 1
             snake_position = snake_position.append(data['board']['snakes'][i]['body'][0]['y'])
 
