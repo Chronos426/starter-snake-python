7fe47cf0de1088106f652268576d0baa9a3be8f5
Timmy <timmynielstim1111@gmail.com>
2020-04-28 17:49:20 +0200

    lo

--- app/Brain.py
+++ app/Brain.py
@@ -92,13 +92,13 @@ def food_path(data, chemin):
 
         if diff_x != 0:
             if diff_x > 0:
-                chemin[0] += float(1/diff_x)
+                chemin[0] += 1
             else:
-                chemin[1] += float(1/abs(diff_x))
+                chemin[1] += 1
 
         if diff_y != 0:
             if diff_y > 0:
-                chemin[2] += float(1/diff_y)
+                chemin[2] += 1
             else:
-                chemin[3] += float(1/abs(diff_y))
+                chemin[3] += 1
     return
