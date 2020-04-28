ac4bd4c01aa6b13857d4f70bedde1e582cbdfe84
Timmy <timmynielstim1111@gmail.com>
2020-04-28 17:47:54 +0200

    lo

--- app/Brain.py
+++ app/Brain.py
@@ -1,9 +1,9 @@
 #data = {"you": {"body": [{"y": 3, "x": 9}, {"y": 4, "x": 9}, {"y": 5, "x": 9}], "health": 94,
-#                "id": "gs_WdqV3GF3Vj6TDbjffScYMgG4", "shout": "", "name": "SnakeOne"}, "board": {
-#    "food": [{"y": 7, "x": 0}, {"y": 7, "x": 2}, {"y": 6, "x": 1}, {"y": 2, "x": 3}, {"y": 5, "x": 1}], "snakes": [
-#        {"body": [{"y": 3, "x": 9}, {"y": 4, "x": 9}, {"y": 5, "x": 9}], "health": 94,
-#         "id": "gs_WdqV3GF3Vj6TDbjffScYMgG4", "shout": "", "name": "SnakeOne"}], "width": 11, "height": 11},
-#       "turn": 6, "game": {"id": "08076fc9-07f0-462c-9ec7-59b3c8fdb08d"}}
+    #            "id": "gs_WdqV3GF3Vj6TDbjffScYMgG4", "shout": "", "name": "SnakeOne"}, "board": {
+   # "food": [{"y": 7, "x": 0}, {"y": 7, "x": 2}, {"y": 6, "x": 1}, {"y": 2, "x": 3}, {"y": 5, "x": 1}], "snakes": [
+   #     {"body": [{"y": 3, "x": 9}, {"y": 4, "x": 9}, {"y": 5, "x": 9}], "health": 94,
+  #       "id": "gs_WdqV3GF3Vj6TDbjffScYMgG4", "shout": "", "name": "SnakeOne"}], "width": 11, "height": 11},
+ #    "turn": 6, "game": {"id": "08076fc9-07f0-462c-9ec7-59b3c8fdb08d"}}
 #chemin = [0, 0, 0, 0]
 
 
@@ -76,23 +76,27 @@ def obstacle_detection(data):
 def food_path(data, chemin):
     head_x_1 = data['you']['body'][0]['x']
     head_y_1 = data['you']['body'][0]['y']
-    food_x = data['board']['food'][0]['x']
-    food_y = data['board']['food'][0]['y']
 
-    diff_x = head_x_1 - food_x
-    diff_y = head_y_1 - food_y
+    nombre_food = len(data['board']['food'])
 
-    if abs(diff_x) >= abs(diff_y):
-        if diff_x > 0:
-            chemin[0] += 1
-            return
+    for i in range(nombre_food):
+        food_x = data['board']['food'][i]['x']
+        food_y = data['board']['food'][i]['y']
+
+        diff_x = head_x_1 - food_x
+        diff_y = head_y_1 - food_y
+
+        if abs(diff_x) >= abs(diff_y):
+            if diff_x > 0:
+                chemin[0] += 1+(1/diff_x)
+                return
+            else:
+                chemin[1] += 1+(1/diff_x)
+                return
         else:
-            chemin[1] += 1
-            return
-    else:
-        if diff_y > 0:
-            chemin[2] += 1
-            return
-        else:
-            chemin[3] += 1
-            return
+            if diff_y > 0:
+                chemin[2] += 1+(1/diff_y)
+                return
+            else:
+                chemin[3] += 1+(1/diff_y)
+                return
